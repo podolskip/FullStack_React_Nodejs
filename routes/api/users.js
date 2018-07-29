fa3a3dc429bc68d62d1de18e26ca95ceaed028c6
Patryk Podolski <podolski.patryk@gmail.com>
2018-07-29 14:04:21 +0200

    011 set up api/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Load user model 
 const User = require('../../models/User');
@@ -55,5 +56,43 @@ router.post('/register', (req, res) => {
             }
         });
 })
+
+// @route       GET api/users/login
+// @desc        Login User / retuening JWT token
+// @access      Public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email
+    User.findOne({ email }) // because od the same name (email: email)
+        .then((user) => {
+            // check if user egsist 
+            if (!user) {
+                return res.status(404).json({ email: 'User not found!' });
+            }
+
+            // check if password is correct
+
+            bcrypt.compare(password, user.password)
+                .then((isMatch) => {
+                    if (isMatch) {
+                        // // user matched 
+                        // const payload = {
+                        //     id: user.id,
+                        //     name: user.name,
+                        //     } // Create jwr Payload
+
+                        // // sign token
+                        // jwt.sign(payload);
+                        res.json({ msg: 'Success' })
+                    } else {
+                        res.status(400).json({ password: 'Password incorrect' })
+                    }
+                })
+
+        })
+});
 
 module.exports = router;
